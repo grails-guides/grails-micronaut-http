f4853f0c5a409fbce43c5dcfeabbaf71439241ba
niravassar <assarn@objectcomputing.com>
2019-05-02 15:27:22 -0500

    use client for get test

--- complete/grails-app/init/example/grails/BootStrap.groovy
+++ complete/grails-app/init/example/grails/BootStrap.groovy
@@ -3,6 +3,8 @@ package example.grails
 class BootStrap {
 
     def init = { servletContext ->
+        new RecordLabel(name:"Warner").save()
+        new RecordLabel(name:"Sony").save()
     }
     def destroy = {
     }
---
+++ complete/src/test/groovy/example/grails/RecordLabelControllerSpec.groovy
@@ -0,0 +1,37 @@
+package example.grails
+
+import grails.gorm.transactions.Rollback
+import grails.testing.mixin.integration.Integration
+import grails.testing.spock.OnceBefore
+import io.micronaut.core.type.Argument
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import spock.lang.Shared
+import spock.lang.Specification
+
+@Integration
+@Rollback
+class RecordLabelControllerSpec extends Specification {
+
+    @Shared
+    HttpClient client
+
+    @OnceBefore
+    void init() {
+        String baseUrl = "http://localhost:$serverPort"
+        this.client  = HttpClient.create(baseUrl.toURL())
+    }
+
+    void "test rest get record labels"() {
+        when:"record labels exist"
+        HttpResponse<List<Map>> resp = client.toBlocking().exchange(HttpRequest.GET("/recordlabels"), Argument.of(List, Map))
+
+        then: "client can retrieve them"
+        resp.status == HttpStatus.OK
+        resp.body().size() == 2
+        resp.body()[0].name == "Warner"
+        resp.body()[1].name == "Sony"
+    }
+}
