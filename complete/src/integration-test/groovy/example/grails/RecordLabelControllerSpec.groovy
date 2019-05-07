1bcd305b27c82d1b45f53ef72362e2721006dc17
niravassar <assarn@objectcomputing.com>
2019-05-07 12:21:45 -0500

    Testing section

--- complete/src/integration-test/groovy/example/grails/RecordLabelControllerSpec.groovy
+++ complete/src/integration-test/groovy/example/grails/RecordLabelControllerSpec.groovy
@@ -19,28 +19,28 @@ class RecordLabelControllerSpec extends Specification {
     HttpClient client
 
     @OnceBefore
-    void init() {
+    void init() { // <1>
         String baseUrl = "http://localhost:$serverPort"
         this.client  = HttpClient.create(baseUrl.toURL())
     }
 
     void "test rest get record labels"() {
         when:"record labels exist"
-        HttpResponse<List<Map>> resp = client.toBlocking().exchange(HttpRequest.GET("/recordlabels"), Argument.of(List, Map))
+        HttpResponse<List<Map>> resp = client.toBlocking().exchange(HttpRequest.GET("/recordlabels"), Argument.of(List, Map)) // <2>
 
         then: "client can retrieve them"
-        resp.status == HttpStatus.OK
-        resp.body().size() == 2
+        resp.status == HttpStatus.OK  // <3>
+        resp.body().size() ==
         resp.body()[0].name == "Warner"
         resp.body()[1].name == "Sony"
     }
 
     void "test rest post record labels"() {
         when:"a post is issued"
-        HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.POST("/recordlabels", [name: "Universal"]), Map)
+        HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.POST("/recordlabels", [name: "Universal"]), Map) // <4>
 
         then: "element is created"
-        resp.status == HttpStatus.CREATED
+        resp.status == HttpStatus.CREATED // <5>
         resp.body().size() == 2
         resp.body().id
         resp.body().name == "Universal"
