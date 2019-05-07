b455e15ddbaad41406a68a317529e40733221640
niravassar <assarn@objectcomputing.com>
2019-05-07 10:23:59 -0500

    low level section

--- complete/grails-app/services/example/grails/ItunesSearchService.groovy
+++ complete/grails-app/services/example/grails/ItunesSearchService.groovy
@@ -14,18 +14,20 @@ class ItunesSearchService {
     @Autowired
     ItunesClient itunesClient
 
+    // tag::searchWithApi[]
     List<Album> searchWithApi(String searchTerm) {
         String baseUrl = "https://itunes.apple.com/"
         HttpClient client = HttpClient.create(baseUrl.toURL())
         HttpRequest request = HttpRequest.GET("/search?limit=25&media=music&entity=album&term=${searchTerm}")
 
-        HttpResponse<String> resp = client.toBlocking().exchange(request, String)
+        HttpResponse<String> resp = client.toBlocking().exchange(request, String) // <1>
         String json = resp.body()
         ObjectMapper objectMapper = new ObjectMapper()
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        SearchResult searchResult = objectMapper.readValue(json, SearchResult)
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) // <2>
+        SearchResult searchResult = objectMapper.readValue(json, SearchResult) // <3>
         searchResult.results
     }
+    // end::searchWithApi[]
 
     List<Album> searchWithDeclarativeClient(String searchTerm) {
         SearchResult searchResult = itunesClient.search(searchTerm)
