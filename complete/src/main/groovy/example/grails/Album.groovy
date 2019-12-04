9178c55aab63b83e5a8030c7d572e6c5e2414f24
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-04 18:31:14 +0100

    annotate with CompileStatic

--- complete/src/main/groovy/example/grails/Album.groovy
+++ complete/src/main/groovy/example/grails/Album.groovy
@@ -1,5 +1,8 @@
 package example.grails
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class Album {
     String artistName
     String collectionName
--- complete/src/main/groovy/example/grails/SearchResult.groovy
+++ complete/src/main/groovy/example/grails/SearchResult.groovy
@@ -1,5 +1,8 @@
 package example.grails
 
+import groovy.transform.CompileStatic
+
+@CompileStatic
 class SearchResult {
     int resultCount
     List<Album> results = []
